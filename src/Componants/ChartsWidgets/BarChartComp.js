898704e2661dd75941d3918770e471ad28c380a6
Ahmed Moustafa <a.moustafa@procoor.local>
2019-01-09 04:20:22 -0800

    try commit

--- src/Componants/ChartsWidgets/BarChartComp.js
+++ src/Componants/ChartsWidgets/BarChartComp.js
@@ -103,7 +103,6 @@ class BarChartComp extends Component {
                 });
 
                 this.setState({ options: { series: { name: this.props.title, data: _data }, xAxis: { categories: _catag } } });
-           
             }
             else {
                 results.map((item) => {
