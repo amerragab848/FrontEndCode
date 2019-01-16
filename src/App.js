db60a27509599eed114e440a1db80c4ab40cfd8d
Ahmed Moustafa <a.moustafa@procoor.local>
2019-01-16 01:32:33 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import "./Styles/scss/en-us/layout.css"; 
 import SendToInbox from  './Componants/OptionsPanels/SendToInbox';
-import DashBoard from  './Pages/DashBoard'; 
 //sayed
 import CreateTransmittal from  './Componants/OptionsPanels/CreateTransmittal';
 import SendTask from  './Componants/OptionsPanels/SendTask';
--- src/Componants/OptionsPanels/DistributionList.js
+++ src/Componants/OptionsPanels/DistributionList.js
@@ -12,6 +12,7 @@ import 'rc-table/assets/animation.css';
 
 
 import moment from 'moment';
+import Index from '../Index';
 const _ = require('lodash')
 const AnimateBody = props => <Animate transitionName="move" component="tbody" {...props} />;
 class DistributionList extends Component {
@@ -33,15 +34,22 @@ class DistributionList extends Component {
                 { title: 'Action', dataIndex: 'a', key: 'a', width: 100 },
                 { title: 'Company Name', dataIndex: 'b', key: 'b', width: 100 },
                 { title: 'Contact Name', dataIndex: 'c', key: 'c', width: 200 },
-                { title: 'Action', dataIndex: 'd', key: 'c', width: 200 }
+                {
+                    title: 'Action',
+                    dataIndex: '',
+                    key: 'd',
+                    render: (text, record) => (
+                      <Dropdown title="" data={this.state.ActionData} handleChange={this.DistributionDate} />
+               
+                    ),
+                    width: 250,
+                }
 
              
             ],
 
             data: [
-                { a: '123', key: '1' },
-                { a: 'cdd', b: 'edd', key: '2' },
-                { a: '1333', c: 'eee', key: '3' },
+             
             ],
 
 
@@ -67,8 +75,8 @@ class DistributionList extends Component {
             PriorityData: [],
             CompanyData: [],
             ContactNameData: [],
-            Cc_ContactData: [],
-            Cc_Selected: [],
+            ActionData:[],
+            DistributionList:[]
 
 
         };
@@ -102,10 +110,10 @@ class DistributionList extends Component {
 
     }
 
-    DistributionDate = (item) => {
-        this.setState({
-            sendingData: { ...this.state.sendingData, priorityId: item.value }
-        })
+    DistributionHanleChange = (item) => {
+        let url="GetProjectDistributionListItemsByDistributionId?distributionId="+item.value;
+        this.GetDistributionData(url);
+        console.log(this.GetDistributionData(url))
     }
 
     Priority_handelChange = (item) => {
@@ -117,9 +125,10 @@ class DistributionList extends Component {
     componentDidMount = () => {
         let url = "getProjectDistributionList?projectId=" + this.state.sendingData.projectId;
         let url2 = "GetProjectProjectsCompaniesForList?projectId=" + this.state.sendingData.projectId;
-        this.GetData(url, 'subject', 'projectId', 'DistributionDate');
+        this.GetData(url, 'subject', 'id', 'DistributionDate');
         this.GetData("GetaccountsDefaultListForList?listType=priority", 'title', 'id', 'PriorityData');
         this.GetData(url2, 'companyName', 'companyId', 'CompanyData');
+        this.GetData("GetaccountsDefaultListForList?listType=distribution_action", 'title', 'id', 'ActionData');
     }
 
 
@@ -133,7 +142,7 @@ class DistributionList extends Component {
 
         return (
             <div className="dropWrapper">
-                <Dropdown title="Distribution List" data={this.state.DistributionDate} handleChange={this.DistributionDate} />
+                <Dropdown title="Distribution List" data={this.state.DistributionDate} handleChange={this.DistributionHanleChange} />
 
                 <DatePicker startDate={this.state.startDate} handleChange={this.DatehandleChange} />
 
@@ -150,15 +159,15 @@ class DistributionList extends Component {
                 <div className="fullWidthWrapper">
                     <h4 className="twoLineHeader">Contact List</h4>
                 </div>
-                {/* <div className="modal-header fullWidthWrapper">
+                <div className="modal-header fullWidthWrapper">
                     <Table
                         columns={this.state.columns}
-                        data={this.state.data}
+                        data={this.state.DistributionList}
                         components={{
                             body: { wrapper: AnimateBody },
                         }}
                     />
-                </div> */}
+                </div>
                 <div className="fullWidthWrapper">
                     <button className="primaryBtn-1 btn">SEND</button>
                 </div>
@@ -191,6 +200,20 @@ class DistributionList extends Component {
             });
 
 
+        }).catch(ex => {
+        });
+    }
+
+    GetDistributionData = (url) => {
+        let data=[]
+        Api.get(url).then(result => {
+            result.map((item,Index)=>{
+                data.push({b:item['companyName'],c:item['contactName'] ,key:Index})
+            })
+       
+        this.setState({DistributionList:[...data]})
+        console.log(this.state.DistributionList)
+            
         }).catch(ex => {
         });
     }
