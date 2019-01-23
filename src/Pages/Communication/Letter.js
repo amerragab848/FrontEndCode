ef28f93df1c8879ce653428f069b47ce05688e40
m.ramdan <m.ramdan@procoor.local>
2019-01-23 13:48:55 +0200

    add grid of common log

--- src/Pages/Communication/Letter.js
+++ src/Pages/Communication/Letter.js
@@ -4,12 +4,17 @@ import GridSetup from  './GridSetup';
  
 import Api from '../../api'
 
+import { Toolbar, Data, Filters } from "react-data-grid-addons";
+import documentDefenition from "../../documentDefenition.json";
 let currentLanguage = localStorage.getItem('lang') == null ? 'en' : localStorage.getItem('lang');
-const defaultColumnProperties = {
-  resizable: true,
-  width: 120
-};
-const ProgressBarFormatter = ({ value }) => {
+ const {
+  NumericFilter,
+  AutoCompleteFilter,
+  MultiSelectFilter,
+  SingleSelectFilter
+} = Filters;
+
+const subjectLink = ({ value }) => {
 	//style="white-space:nowrap;overflow:hidden;text-overflow:ellipsis"
   return <div id="divSubject" ><a data-bind="attr: { href: '#lettersAddEdit/' + $parent.entity.id + '/' + $parent.entity.projectId + '/' + undefined  + '/' + undefined + '/' + undefined + '/' + $parent.entity.projectName }"><span>{`${value}%`}</span></a></div>;
 };
@@ -18,42 +23,50 @@ class Letter extends Component {
   
     constructor(props) {
         super(props)
+        let documentObj=documentDefenition['Letters'];
+        let cNames=[];
+        
+        console.log(documentObj);
+        documentObj.documentColumns.map(item=>{
+        	
+        	if(item.isCustom===true){
+	        	var obj={
+	        		key: item.field,
+	        		name: item.friendlyName,
+	        		width: item.minWidth,
+			        draggable: true,
+	  				sortable: true,
+			        resizable: true,
+	 				filterable: true,
+	    			sortDescendingFirst: true, 
+	    			filterRenderer: item.dataType === 'number' ? NumericFilter: SingleSelectFilter 
+	        	};
+	        	cNames.push(obj);
+        	}
+        });
+        
+        console.log(cNames);
+
         this.state = { 
         	projectId: 4330,
-            rows: [{arange:'1',subject:'subject 001',statusName: 'opened',docDate:'2018/12/31'}],
-            columns:[
-					  {
-					    key: "arrange",
-					    name: "arrange"
-					  },
-					  {
-					    key: "subject",
-					    name: "subject", formatter: ProgressBarFormatter 
-					  },
-					  {
-					    key: "statusName",
-					    name: "status"
-					  },
-					  {
-					    key: "docDate",
-					    name: "Document Date"
-					  } 
-					],
+        	docType: 'Letters',
+            rows: [],
+            columns:cNames, 
 			pageSize: 50,
-			isLoading: true		
+			isLoading: true	,
+			api: documentObj.documentApi.get,
+			pageNumber:0
+
         } 
     }
 
-	componentWillMount = () => {
-
-	    let url = "GetLettersByProjectIdCustom?projectId=" + this.state.projectId+'&pageNumber=0&pageSize=100';
+	componentWillMount = () => {  
+	    let url = this.state.api+"?projectId=" + this.state.projectId+'&pageNumber='+ this.state.pageNumber+'&pageSize='+ this.state.pageSize;
 	    this.GetLogData(url, 'rows');
 	}
 
-	GetLogData = (url, currState) => {
-	    
-	    Api.get(url).then(result => {
-	         
+	GetLogData = (url, currState) => { 
+	    Api.get(url).then(result => { 
 	        console.log('loading data....');
 	        this.setState({
 	            rows: result,
@@ -69,8 +82,8 @@ class Letter extends Component {
 
   render() { 
 
-  	let columns=this.state.columns.map(c => ({ ...c, ...defaultColumnProperties }));
-  	const dataGrid = this.state.isLoading===false ? <GridSetup rows={this.state.rows} pageSize={this.state.pageSize}  columns={columns} /> :null;
+  	//let columns=this.state.columns;//.map(c => ({ ...c, ...defaultColumnProperties }));
+  	const dataGrid = this.state.isLoading===false ? <GridSetup rows={this.state.rows} pageSize={this.state.pageSize}  columns={this.state.columns} /> :null;
   	return( 
   		<div>
   		{dataGrid}
