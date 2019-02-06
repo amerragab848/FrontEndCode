c7ff605750fa0535f3257aed509c7a04a0ba1008
m.ramdan <m.ramdan@procoor.local>
2019-02-06 13:24:24 +0200

    add classs cursor css

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import React, { Component } from "react";
 import "./Styles/scss/en-us/layout.css";
 import "./Styles/scss/en-us/reactCss.css";
 
-import CreateTransmittal from "./Componants/OptionsPanels/CreateTransmittal";
-import SendTask from "./Componants/OptionsPanels/SendTask";
-import Letter from "./Pages/Communication/Letter";
-import DistributionList from "./Componants/OptionsPanels/DistributionList";
-import Filter from "./Componants/FilterComponent/filterComponent";
-import TimeSheet from "./Componants/DashBoardDetails/TimeSheetDetails";
-import SendWorkFlow from './Componants/OptionsPanels/SendWorkFlow';
+// import CreateTransmittal from "./Componants/OptionsPanels/CreateTransmittal";
+// import SendTask from "./Componants/OptionsPanels/SendTask";
+// import DistributionList from "./Componants/OptionsPanels/DistributionList";
+// import Filter from "./Componants/FilterComponent/filterComponent";
+// import TimeSheet from "./Componants/DashBoardDetails/TimeSheetDetails";
+// import SendWorkFlow from './Componants/OptionsPanels/SendWorkFlow';
 
-import ViewAttachment from './Componants/OptionsPanels/ViewAttachmments'
-import UploadAttachment from './Componants/OptionsPanels/UploadAttachment'
-import Upload from './Componants/OptionsPanels/UploadAttachment'
-import WFApproval from './Componants/OptionsPanels/wfApproval'
-import SendToInbox from './Componants/OptionsPanels/SendToInbox'
+// import ViewAttachment from './Componants/OptionsPanels/ViewAttachmments'
+// import UploadAttachment from './Componants/OptionsPanels/UploadAttachment'
+// import Upload from './Componants/OptionsPanels/UploadAttachment'
+// import WFApproval from './Componants/OptionsPanels/wfApproval'
+// import SendToInbox from './Componants/OptionsPanels/SendToInbox'
 import PrivacySetting from './Componants/User/PrivacySetting '
 
 
@@ -29,6 +28,7 @@ import SendByEmails from "./Componants/OptionsPanels/SendByEmails";
 import WfApproval from './Componants/OptionsPanels/wfApproval';
 
 
+import Letter from "./Pages/Communication/Letter";
 
 import PopUp from './Componants/OptionsPanels/OptionContainer'
 import Login from './Componants/Layouts/Login'
