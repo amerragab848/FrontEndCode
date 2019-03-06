99dd4c0b67f8e3d250ebe2bce8706e16aa7308d9
Sayed Abdulhameed <s.abdulhameed@procoor.local>
2019-03-06 18:46:29 +0200

    Validation generalList

--- src/Componants/GeneralSetting/Project/ExpensesWorkFlow/ExpensesWorkFlowAddEdit.js
+++ src/Componants/GeneralSetting/Project/ExpensesWorkFlow/ExpensesWorkFlowAddEdit.js
@@ -8,24 +8,48 @@ import Export from "../../../../Componants/OptionsPanels/Export";
 import config from "../../../../Services/Config";
 import Resources from "../../../../resources.json";
 import Api from '../../../../api';
+import dataservice from "../../../../Dataservice";
+import DropDown from '../../../OptionsPanels/DropdownMelcous'
+import DatePicker from '../../../OptionsPanels/DatePicker'
+import Contacts, { MultiApproval } from '../ExpensesWorkFlow/ExpensesWorkFlow'
+import moment from 'moment';
 let currentLanguage = localStorage.getItem("lang") == null ? "en" : localStorage.getItem("lang");
-
+let CurrProject = localStorage.getItem('lastSelectedprojectName')
 class ExpensesWorkFlowAddEdit extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            ActiveStep:false
+            ActiveStep: false , 
+            ProjectList:[],
+            CompanyData:[],
+            ContactData:[]
+
         }
     }
     componentDidMount = () => {
-
+          dataservice.GetDataList('ProjectProjectsGetAll', 'projectName', 'id').then(
+                 res => {
+                     this.setState({
+                        ProjectList: res,
+                     })
+                 }
+             )
+             dataservice.GetDataList('GetProjectCompanies?accountOwnerId=2', 'projectName', 'id').then(
+                res => {
+                    this.setState({
+                        CompanyData: res,
+                    })
+                }
+            )
     }
     render() {
+
         return (
             <div className="mainContainer">
+
                 <div className="documents-stepper noTabs__document one__tab one_step">
                     <div className="submittalHead">
-                        <h2 className="zero">Expenses WorkFlow </h2>
+                        <h2 className="zero">{CurrProject + ' - ' + Resources['expensesWorkFlow'][currentLanguage]}</h2>
                         <div className="SubmittalHeadClose">
                             <svg width="56px" height="56px" viewBox="0 0 56 56" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
                                 <g id="Symbols" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
@@ -48,7 +72,66 @@ class ExpensesWorkFlowAddEdit extends Component {
                     </div>
                     <div className="doc-container">
                         <div className="step-content">
-                            
+                            {!this.state.ActiveStep ?
+                                <div className="document-fields">
+                                    <form className="proForm first-proform">
+                                        <div className="linebylineInput valid-input">
+                                            <label className="control-label">Subject</label>
+                                            <div className="inputDev ui input">
+                                                <input autoComplete="off" type="text" className="form-control fsadfsadsa" id="firstname1" name="firstname1" placeholder="" />
+                                            </div>
+                                        </div>
+                                        <div className="linebylineInput">
+                                            <label className="control-label"> {Resources['status'][currentLanguage]} </label>
+                                            <div className="ui checkbox radio radioBoxBlue">
+                                                <input type="radio" name="designTeam" value="true" onChange={this.DesignTeamChange} />
+                                                <label>{Resources['oppened'][currentLanguage]}</label>
+                                            </div>
+                                            <div className="ui checkbox radio radioBoxBlue checked">
+                                                <input type="radio" defaultChecked name="designTeam" value="false" onChange={this.DesignTeamChange} />
+                                                <label> {Resources['closed'][currentLanguage]}</label>
+                                            </div>
+                                        </div>
+                                    </form>
+                                    <form className="proForm datepickerContainer">
+                                        <div className="linebylineInput valid-input">
+                                            <div className="inputDev ui input">
+                                                <DropDown data={this.state.ProjectList} />
+                                            </div>
+                                        </div>
+
+                                        <div className="linebylineInput valid-input">
+                                            <div className="inputDev ui input">
+                                                <DatePicker startDate={moment().format('DD:MM:YYYY')}/>
+                                            </div>
+                                        </div>
+
+                                        <div className="linebylineInput valid-input">
+                                            <label className="control-label">NO.</label>
+                                            <div className="inputDev ui input">
+                                                <input autoComplete="off"  type="text" className="form-control fsadfsadsa" id="firstname1" name="firstname1" placeholder="" />
+                                            </div>
+                                        </div>
+
+                                    </form>
+
+                                </div>
+                                :
+                                <div className="doc-pre-cycle">
+
+                                    <Contacts />
+                                </div>
+                            }
+                            <div className="doc-pre-cycle">
+
+                                <div className="slider-Btns">
+                                    <button className="primaryBtn-1 btn meduimBtn" onClick={() => this.setState({ ActiveStep: true })}>NEXT STEP</button>
+                                </div>
+                            </div>
+
+
+
+
                         </div>
 
                         <div className="docstepper-levels">
@@ -61,22 +144,22 @@ class ExpensesWorkFlowAddEdit extends Component {
 
                             <div className="workflow-sliderSteps">
                                 <div className="step-slider">
-                                    <div data-id="step1" className= "step-slider-item  first-stepOne active">
+                                    <div data-id="step1" className="step-slider-item  first-stepOne active">
                                         <div className="steps-timeline">
                                             <span>1</span>
                                         </div>
                                         <div className="steps-info">
-                                            <h6>Basic info</h6>
-                                          
+                                            <h6>Expenses WorkFlow</h6>
+
                                         </div>
                                     </div>
-                                   
-                                    <div data-id="step2" className={this.state.ActiveStep? "step-slider-item  active" : "step-slider-item"}>
+
+                                    <div data-id="step2" className={this.state.ActiveStep ? "step-slider-item  active" : "step-slider-item"}>
                                         <div className="steps-timeline">
                                             <span>2</span>
                                         </div>
                                         <div className="steps-info">
-                                            <h6>Basic info</h6>
+                                            <h6>Contacts</h6>
                                         </div>
                                     </div>
                                     <div data-id="step3" className="step-slider-item">
@@ -84,17 +167,17 @@ class ExpensesWorkFlowAddEdit extends Component {
                                             <span>3</span>
                                         </div>
                                         <div className="steps-info">
-                                            <h6>Basic info</h6>
+                                            <h6>Multi Approval</h6>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                 
-                    <div className="slider-Btns">
-                                            <button className="primaryBtn-1 btn meduimBtn" onClick={()=>this.setState({ActiveStep:true})}>NEXT STEP</button>
-                                        </div>
+
+
+
+
                 </div>
             </div>
         )
