5e4e0aeaf27955e88869e8bc262057963c199dca
m.ramdan <m.ramdan@procoor.local>
2019-03-10 18:05:39 +0200

    add validation form in letter

--- src/Componants/OptionsPanels/DropdownMelcous.js
+++ src/Componants/OptionsPanels/DropdownMelcous.js
@@ -12,10 +12,10 @@ class DropdownMelcous extends Component {
         this.props.handleChange(e,'sokary')
     }
 
-    // handleBlur = () => {
-    //     this.props.onBlur("topics", true);
-    // };
-    
+    handleBlur = (e) => { 
+        this.props.handleBlur(this.props.name, true);
+    }
+
     render() { 
 
         return (
@@ -30,25 +30,20 @@ class DropdownMelcous extends Component {
                 <div>
                     <div className="customD_Menu" style={{ outline: "none" }}>
                         <Select key={this.props.index} ref={this.props.index}
-                            name="form-field-name" 
-                            
-                            //onBlur={this.props.handleBlur}
-
-                            onChange={this.props.handleChange}
+                             
                             options={this.props.data}
                             placeholder={this.props.title ? Resources[this.props.title][currentLanguage] : ""}
                             isSearchable="true"
                             defaultValue= {this.props.isMulti ? this.props.selectedValue : this.props.value}
                             value={ this.props.isMulti ? this.props.value : this.props.selectedValue}
                             isMulti={this.props.isMulti}
-                            onBlur={this.props.onblur}
+
+                            name={this.props.name ? this.props.index: this.props.name} 
+                            id={this.props.id ? this.props.index: this.props.id} 
+                            onChange={this.props.handleChange}
+                            onBlur={this.props.handleBlur}
                         />
-                        
-                        {/* {!!this.props.error && this.props.touched && (
-                        <div style={{ color: "red", marginTop: ".5rem" }}>
-                            {this.props.error}
-                        </div>
-                        )} */}
+                          
                     </div>
                 </div>
             </div>
