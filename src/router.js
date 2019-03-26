7bc7c1ded2b66d11a708fbba33ff1d259fb1c2c0
Ahmed Moustafa <a.moustafa@procoor.local>
2019-03-26 00:55:44 -0700

    getting pull

--- src/router.js
+++ src/router.js
@@ -65,6 +65,7 @@ import projectDistributionListAddEdit  from './Pages/ProjectSetup/DistributionLi
 import NCRAddEdit from "./Pages/QualityControl/NCRAddEdit";
 import clientSelectionAddEdit from "./Pages/TechnicalOffice/ClientSelectionAddEdit";
 import clientModificationAddEdit from "./Pages/TechnicalOffice/clientModificationAddEdit";
+import siteInstructions from "./Pages/TechnicalOffice/siteInstructions";
 
 
 let setupRoutes = ProjectSetupRoutes.map((item) => {
@@ -144,6 +145,7 @@ let originalRoutes = [
     , <Route path="/NCRAddEdit" component={NCRAddEdit} />
     , <Route path="/clientSelectionAddEdit" component={clientSelectionAddEdit} />
     , <Route path="/clientModificationAddEdit" component={clientModificationAddEdit} />
+    , <Route path="/siteInstructions" component={siteInstructions} />
     
 ];
 originalRoutes = [...originalRoutes, ...setupRoutes]
