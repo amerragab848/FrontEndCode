7f21ed72700985cf23b5bf3a774c89928b5a3ad1
Ahmed Moustafa <a.moustafa@procoor.local>
2019-04-11 09:45:44 -0700

    adding itemization component

--- src/store/reducers/communication.js
+++ src/store/reducers/communication.js
@@ -56,6 +56,18 @@ export default function (state = initialState.app.communication, action) {
                 docId,
                 items: [...state.items, ...action.item]
             };
+        case types.edit_item:
+            let updateRow = action.item;
+            let items = []
+            state.items.forEach(item => {
+                if (item.id == updateRow.id)
+                    item = updateRow
+                items.push(item)
+            })
+            return {
+                ...state,
+                items
+            };
 
         case types.delete_item:
             let originalData = state.items;
@@ -65,9 +77,10 @@ export default function (state = initialState.app.communication, action) {
                 originalData.splice(getIndex, 1);
 
             });
+            case types.delete_items:
             return {
                 ...state,
-                items: [...originalData]
+                items: []
             };
 
         case types.Delete_File:
