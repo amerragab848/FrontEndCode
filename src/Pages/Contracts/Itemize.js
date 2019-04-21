fe7f7c5da1875638650f6a079a582ad13f5cfe00
Ahmed Moustafa <a.moustafa@procoor.local>
2019-04-21 05:40:28 -0700

    adding headerComponent to contract module

--- src/Pages/Contracts/Itemize.js
+++ src/Pages/Contracts/Itemize.js
@@ -25,7 +25,7 @@ import GridSetup from "../Communication/GridSetup";
 import XSLfile from '../../Componants/OptionsPanels/XSLfiel'
 import IPConfig from '../../IP_Configrations'
 import SkyLight from 'react-skylight';
-
+import HeaderDocument from '../../Componants/OptionsPanels/HeaderDocument'
 let currentLanguage = localStorage.getItem('lang') == null ? 'en' : localStorage.getItem('lang');
 
 
@@ -514,29 +514,7 @@ class Itemize extends Component {
             <React.Fragment>
                 <div className="mainContainer">
                     <div className={this.state.isViewMode === true && this.state.CurrStep != 3 ? "documents-stepper noTabs__document one__tab one_step readOnly_inputs" : "documents-stepper noTabs__document one__tab one_step"}>
-                        <div className="submittalHead">
-                            <h2 className="zero">{Resources.contract[currentLanguage]}
-                                <span>{this.state.projectName ? this.state.projectName.replace(/_/gi, ' ') : ''} {Resources.contracts[currentLanguage]} {Resources.itemize[currentLanguage]} </span>
-                            </h2>
-                            <div className="SubmittalHeadClose">
-                                <svg width="56px" height="56px" viewBox="0 0 56 56" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-                                    <g id="Symbols" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                                        <g id="Components/Sections/Doc-page/Title/Base" transform="translate(-1286.000000, -24.000000)">
-                                            <g id="Group-2">
-                                                <g id="Action-icons/Close/Circulated/56px/Light-grey_Normal" transform="translate(1286.000000, 24.000000)">
-                                                    <g id="Action-icons/Close/Circulated/20pt/Grey_Normal">
-                                                        <g id="Group">
-                                                            <circle id="Oval" fill="#E9ECF0" cx="28" cy="28" r="28"></circle>
-                                                            <path d="M36.5221303,34.2147712 C37.1592899,34.8519308 37.1592899,35.8849707 36.5221303,36.5221303 C35.8849707,37.1592899 34.8519308,37.1592899 34.2147712,36.5221303 L28,30.3073591 L21.7852288,36.5221303 C21.1480692,37.1592899 20.1150293,37.1592899 19.4778697,36.5221303 C18.8407101,35.8849707 18.8407101,34.8519308 19.4778697,34.2147712 L25.6926409,28 L19.4778697,21.7852288 C18.8407101,21.1480692 18.8407101,20.1150293 19.4778697,19.4778697 C20.1150293,18.8407101 21.1480692,18.8407101 21.7852288,19.4778697 L28,25.6926409 L34.2147712,19.4778697 C34.8519308,18.8407101 35.8849707,18.8407101 36.5221303,19.4778697 C37.1592899,20.1150293 37.1592899,21.1480692 36.5221303,21.7852288 L30.3073591,28 L36.5221303,34.2147712 Z" id="Combined-Shape" fill="#858D9E" fillRule="nonzero"></path>
-                                                        </g>
-                                                    </g>
-                                                </g>
-                                            </g>
-                                        </g>
-                                    </g>
-                                </svg>
-                            </div>
-                        </div>
+                <HeaderDocument projectName={projectName} docTitle={Resources.contract[currentLanguage]} moduleTitle={Resources['contracts'][currentLanguage]} />
                         <div className="doc-container">
                             <div className="step-content">
                                 {this.state.LoadingPage ? <LoadingSection /> :
