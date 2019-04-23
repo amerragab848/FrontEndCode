0e90380ee4a2ff4a2ca3c8e3c3c1d5d52615d822
Sayed Abdulhameed <s.abdulhameed@procoor.local>
2019-04-23 16:30:06 +0200

    Other Reports Join to left Menu

--- src/Pages/ReportsCenter/OtherReports/FollowUpUsageReport.js
+++ src/Pages/ReportsCenter/OtherReports/FollowUpUsageReport.js
@@ -22,7 +22,7 @@ class WFUsageReport extends Component {
             dropDownList: [],
             selectedWF: { label: Resources.slectWorkFlow[currentLanguage], value: "0" },
             rows: [],
-            columns : [
+            columns: [
                 {
                     key: "subject",
                     name: Resources["subject"][currentLanguage],
@@ -32,7 +32,7 @@ class WFUsageReport extends Component {
                     resizable: true,
                     filterable: true,
                     sortDescendingFirst: true
-                } , {
+                }, {
                     key: "level",
                     name: Resources["levelNo"][currentLanguage],
                     width: 120,
@@ -43,21 +43,16 @@ class WFUsageReport extends Component {
                     sortDescendingFirst: true
                 },
             ]
-    
+
         }
 
-        if (!Config.IsAllow(3750) ) {
+        if (!Config.IsAllow(3750)) {
             toast.success(Resources["missingPermissions"][currentLanguage]);
             this.props.history.push({
                 pathname: "/"
             });
         }
-   
-    }
 
-
-
-    componentDidMount() {
     }
 
     componentWillMount() {
@@ -74,57 +69,55 @@ class WFUsageReport extends Component {
         })
     }
 
-    DropdownChange=(event)=>{
-        this.setState({ selectedWF: event,isLoading:true })
-        Api.post('GetFollowUpsUsageParent?code='+event.value).then(res=>{
-            let columns=[  
+    DropdownChange = (event) => {
+        this.setState({ selectedWF: event, isLoading: true })
+
+        Api.post('GetFollowUpsUsageParent?code=' + event.value).then(res => {
+
+            let columns = [
                 {
-                key: "subject",
-                name: Resources["subject"][currentLanguage],
-                width: 150,
-                draggable: true,
-                sortable: true,
-                resizable: true,
-                filterable: true,
-                sortDescendingFirst: true
-            } , {
-                key: "level",
-                name: Resources["levelNo"][currentLanguage],
-                width: 120,
-                draggable: true,
-                sortable: true,
-                resizable: true,
-                filterable: true,
-                sortDescendingFirst: true
-            }, {
-                key: "level",
-                name: Resources["levelNo"][currentLanguage],
-                width: 120,
-                draggable: true,
-                sortable: true,
-                resizable: true,
-                filterable: true,
-                sortDescendingFirst: true
-            }]
-            res.forEach((element)=>{
+                    key: "subject",
+                    name: Resources["subject"][currentLanguage],
+                    width: 150,
+                    draggable: true,
+                    sortable: true,
+                    resizable: true,
+                    filterable: true,
+                    sortDescendingFirst: true
+                }, {
+                    key: "level",
+                    name: Resources["levelNo"][currentLanguage],
+                    width: 120,
+                    draggable: true,
+                    sortable: true,
+                    resizable: true,
+                    filterable: true,
+                    sortDescendingFirst: true
+                }, {
+                    key: "level",
+                    name: Resources["levelNo"][currentLanguage],
+                    width: 120,
+                    draggable: true,
+                    sortable: true,
+                    resizable: true,
+                    filterable: true,
+                    sortDescendingFirst: true
+                }]
+
+            res.forEach((element) => {
                 columns.push(
                     {
                         key: element.projectName,
                         name: element.projectName,
-                        width: 120,
-                        draggable: true,
-                        sortable: true,
-                        resizable: true,
-                        filterable: true,
-                        sortDescendingFirst: true
+                        width: 120, draggable: true, sortable: true,
+                        resizable: true, filterable: true, sortDescendingFirst: true
                     })
-              
             })
-       
-        this.setState({columns,isLoading:false, rows: []})
 
+            this.setState({ columns, isLoading: false, rows: [] })
         })
     }
+
     getGridRows = () => {
         if (this.state.selectedWF.value != '0') {
             this.setState({ isLoading: true })
@@ -133,75 +126,39 @@ class WFUsageReport extends Component {
             }).catch(() => {
                 this.setState({ isLoading: false })
             })
-
-
         }
     }
+
     render() {
+
         const dataGrid = this.state.isLoading === false ? (
-            <GridSetup
-                rows={this.state.rows}
-                showCheckbox={false}
-                pageSize={this.state.pageSize}
-                columns={this.state.columns}
-            />) : <LoadingSection />;
+            <GridSetup rows={this.state.rows} showCheckbox={false}
+                pageSize={this.state.pageSize} columns={this.state.columns} />)
+            : <LoadingSection />
+
         const btnExport = this.state.isLoading === false ?
             <Export rows={this.state.isLoading === false ? this.state.rows : []} columns={this.state.columns} fileName={'followUpsUsageReport'} />
-            : null;
+            : null
+
         return (
 
-            <div className='mainContainer main__fulldash'>
-                <div className="documents-stepper noTabs__document">
-                    <div className="submittalHead">
-                        <h2 className="zero">{Resources['followUpsUsageReport'][currentLanguage]}</h2>
-                        <div className="SubmittalHeadClose">
-                            <svg width="56px" height="56px" viewBox="0 0 56 56" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnslink="http://www.w3.org/1999/xlink">
-                                <g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                    <g id="Components/Sections/Doc-page/Title/Base" transform="translate(-1286.000000, -24.000000)">
-                                        <g id="Group-2">
-                                            <g id="Action-icons/Close/Circulated/56px/Light-grey_Normal" transform="translate(1286.000000, 24.000000)">
-                                                <g id="Action-icons/Close/Circulated/20pt/Grey_Normal"><g id="Group"><circle id="Oval" fill="#E9ECF0" cx="28" cy="28" r="28"></circle>
-                                                    <path d="M36.5221303,34.2147712 C37.1592899,34.8519308 37.1592899,35.8849707 36.5221303,36.5221303 C35.8849707,37.1592899 34.8519308,37.1592899 34.2147712,36.5221303 L28,30.3073591 L21.7852288,36.5221303 C21.1480692,37.1592899 20.1150293,37.1592899 19.4778697,36.5221303 C18.8407101,35.8849707 18.8407101,34.8519308 19.4778697,34.2147712 L25.6926409,28 L19.4778697,21.7852288 C18.8407101,21.1480692 18.8407101,20.1150293 19.4778697,19.4778697 C20.1150293,18.8407101 21.1480692,18.8407101 21.7852288,19.4778697 L28,25.6926409 L34.2147712,19.4778697 C34.8519308,18.8407101 35.8849707,18.8407101 36.5221303,19.4778697 C37.1592899,20.1150293 37.1592899,21.1480692 36.5221303,21.7852288 L30.3073591,28 L36.5221303,34.2147712 Z" id="Combined-Shape" fill="#858D9E" fill-rule="nonzero">
-                                                    </path>
-                                                </g>
-                                                </g>
-                                            </g>
-                                        </g>
-                                    </g>
-                                </g>
-                            </svg>
-                        </div>
+            <div className="reports__content">
+                <header>
+                    <h2 className="zero">{Resources.followUpsUsageReport[currentLanguage]}</h2>
+                    {btnExport}
+                </header>
+                <div className='proForm reports__proForm'>
+                    <div className="linebylineInput valid-input">
+                        <Dropdown title="workFlow" data={this.state.dropDownList}
+                            selectedValue={this.state.selectedWF} name="workFlows"
+                            handleChange={event => this.DropdownChange(event)} index="workFlows" />
                     </div>
-                    <div className="doc-container">
-                        <div className="step-content">
-                            <div className="document-fields">
-                                <div className=" fullWidthWrapper textRight">
-                                    {btnExport}
-                                </div>
-                                <div className="proForm datepickerContainer">
-                                    <Dropdown className='fullWidthWrapper'
-                                        title="workFlow"
-                                        data={this.state.dropDownList}
-                                        selectedValue={this.state.selectedWF}
-                                        handleChange={event =>this.DropdownChange(event)}
-                                        name="workFlows"
-                                        index="workFlows"
-                                    />
-                                    <div className="fullWidthWrapper ">
-                                        <button className="primaryBtn-1 btn mediumBtn" onClick={() => this.getGridRows()}>{Resources['search'][currentLanguage]}</button>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="doc-pre-cycle letterFullWidth">
-                                {dataGrid}
-                            </div>
-
-                        </div>
-                    </div>
+                    <button className="primaryBtn-1 btn smallBtn" onClick={() => this.getGridRows()}>{Resources['search'][currentLanguage]}</button>
                 </div>
-
-            </div >
+                <div className="doc-pre-cycle letterFullWidth">
+                    {dataGrid}
+                </div>
+            </div>
         )
     }
 
--- src/Pages/ReportsCenter/OtherReports/WFActivityReport.js
+++ src/Pages/ReportsCenter/OtherReports/WFActivityReport.js
@@ -25,7 +25,7 @@ class WFActivityReport extends Component {
             rows: []
         }
 
-        if (!Config.IsAllow(4017) ) {
+        if (!Config.IsAllow(4017)) {
             toast.success(Resources["missingPermissions"][currentLanguage]);
             this.props.history.push({
                 pathname: "/"
@@ -121,7 +121,7 @@ class WFActivityReport extends Component {
         ];
 
     }
- 
+
     componentDidMount() {
     }
 
@@ -139,7 +139,7 @@ class WFActivityReport extends Component {
             toast.error('somthing wrong')
         })
     }
- 
+
     getGridRows = () => {
         if (this.state.selectedEmployee.value != '0') {
             this.state.employeesList.forEach(employee => {
@@ -155,73 +155,42 @@ class WFActivityReport extends Component {
 
         }
     }
-  
+
     render() {
+
         const dataGrid = this.state.isLoading === false ? (
-            <GridSetup
-                rows={this.state.rows}
-                showCheckbox={false}
-                pageSize={this.state.pageSize}
-                columns={this.columns}
-            />) : <LoadingSection />;
+            <GridSetup rows={this.state.rows} showCheckbox={false}
+                pageSize={this.state.pageSize} columns={this.columns} />) : <LoadingSection />;
+
+
         const btnExport = this.state.isLoading === false ?
             <Export rows={this.state.isLoading === false ? this.state.rows : []} columns={this.columns} fileName={'workFlowActivity'} />
-            : null;
+            : null
+
         return (
 
-            <div className='mainContainer main__fulldash'>
-                <div className="documents-stepper noTabs__document">
-                    <div className="submittalHead">
-                        <h2 className="zero">{Resources['workFlowActivity'][currentLanguage]}</h2>
-                        <div className="SubmittalHeadClose">
-                            <svg width="56px" height="56px" viewBox="0 0 56 56" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnslink="http://www.w3.org/1999/xlink">
-                                <g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                    <g id="Components/Sections/Doc-page/Title/Base" transform="translate(-1286.000000, -24.000000)">
-                                        <g id="Group-2">
-                                            <g id="Action-icons/Close/Circulated/56px/Light-grey_Normal" transform="translate(1286.000000, 24.000000)">
-                                                <g id="Action-icons/Close/Circulated/20pt/Grey_Normal"><g id="Group"><circle id="Oval" fill="#E9ECF0" cx="28" cy="28" r="28"></circle>
-                                                    <path d="M36.5221303,34.2147712 C37.1592899,34.8519308 37.1592899,35.8849707 36.5221303,36.5221303 C35.8849707,37.1592899 34.8519308,37.1592899 34.2147712,36.5221303 L28,30.3073591 L21.7852288,36.5221303 C21.1480692,37.1592899 20.1150293,37.1592899 19.4778697,36.5221303 C18.8407101,35.8849707 18.8407101,34.8519308 19.4778697,34.2147712 L25.6926409,28 L19.4778697,21.7852288 C18.8407101,21.1480692 18.8407101,20.1150293 19.4778697,19.4778697 C20.1150293,18.8407101 21.1480692,18.8407101 21.7852288,19.4778697 L28,25.6926409 L34.2147712,19.4778697 C34.8519308,18.8407101 35.8849707,18.8407101 36.5221303,19.4778697 C37.1592899,20.1150293 37.1592899,21.1480692 36.5221303,21.7852288 L30.3073591,28 L36.5221303,34.2147712 Z" id="Combined-Shape" fill="#858D9E" fill-rule="nonzero">
-                                                    </path>
-                                                </g>
-                                                </g>
-                                            </g>
-                                        </g>
-                                    </g>
-                                </g>
-                            </svg>
-                        </div>
-                    </div>
-                    <div className="doc-container">
-                        <div className="step-content">
-                            <div className="document-fields">
-                                <div className=" fullWidthWrapper textRight">
-                                    {btnExport}
-                                </div>
-                                <div className="proForm datepickerContainer">
-                                    <Dropdown className='fullWidthWrapper textLeft' 
-                                        title="employee"
-                                        data={this.state.dropDownList}
-                                        selectedValue={this.state.selectedEmployee}
-                                        handleChange={event => this.setState({ selectedEmployee: event })}
-                                        name="employees"
-                                        index="employees"
-                                    />
-                                    <div className="fullWidthWrapper ">
-                                        <button className="primaryBtn-1 btn mediumBtn" onClick={() => this.getGridRows()}>{Resources['search'][currentLanguage]}</button>
-                                    </div>
-                                </div>
 
-                            </div>
-                            <div className="doc-pre-cycle letterFullWidth">
-                                {dataGrid}
-                            </div>
-
-                        </div>
+            <div className="reports__content">
+                <header>
+                    <h2 className="zero">{Resources.workFlowActivity[currentLanguage]}</h2>
+                    {btnExport}
+                </header>
+                <div className='proForm reports__proForm'>
+                    <div className="linebylineInput valid-input">
+                        <Dropdown title="employee" data={this.state.dropDownList} index="employees"
+                            selectedValue={this.state.selectedEmployee} name="employees"
+                            handleChange={event => this.setState({ selectedEmployee: event })} />
                     </div>
+
+                    <button className="primaryBtn-1 btn smallBtn" onClick={() => this.getGridRows()}>{Resources['search'][currentLanguage]}</button>
                 </div>
+                <div className="doc-pre-cycle letterFullWidth">
+                    {dataGrid}
+                </div>
+            </div>
 
-            </div >
-        )
+
+     )
     }
 
 }
