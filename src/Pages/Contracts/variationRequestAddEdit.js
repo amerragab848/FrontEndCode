bc2bde982cff9fa10e6f1e666fb989060d9631e1
m.ramdan <m.ramdan@procoor.local>
2019-04-29 14:40:54 +0200

    hide send WF panel after finish

--- src/Pages/Contracts/variationRequestAddEdit.js
+++ src/Pages/Contracts/variationRequestAddEdit.js
@@ -423,20 +423,20 @@ class VariationRequestAdd extends Component {
         )
     }
 
-    // handleShowAction = (item) => { 
+    handleShowAction = (item) => { 
         if (item.title == "sendToWorkFlow") { this.props.actions.SendingWorkFlow(true); }
-    //     console.log(item);
-    //     if (item.value != "0") {
+ 
+        if (item.value != "0") {
 
-    //         this.setState({
-    //             currentComponent: item.value,
-    //             currentTitle: item.title,
-    //             showModal: true
-    //         })
+            this.setState({
+                currentComponent: item.value,
+                currentTitle: item.title,
+                showModal: true
+            })
 
-    //         this.simpleDialog.show()
-    //     }
-    // }
+            this.simpleDialog.show()
+        }
+    }
 
     render() {
 
