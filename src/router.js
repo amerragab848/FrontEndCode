451ac18d9e8c945fbcde29a55305ab49f7229daa
Ahmed Moustafa <a.moustafa@procoor.local>
2019-05-12 03:43:58 -0700

    add contractInfo component to contract

--- src/router.js
+++ src/router.js
@@ -72,6 +72,7 @@ import siteInstructionsAddEdit from "./Pages/TechnicalOffice/siteInstructionsAdd
 import punchListAddEdit from "./Pages/QualityControl/punchListAddEdit";
 import boqAddEdit from "./Pages/Contracts/boqAddEdit";
 import SubContract from "./Pages/Contracts/SubContract";
+import ContractInfo from "./Pages/Contracts/ContractInfo";
 import ContractsConditions from "./Pages/Contracts/ContractsConditions";
 import variationOrderAddEdit from "./Pages/Contracts/variationOrderAddEdit";
 import addEditModificationDrawing from "./Pages/Design/addEditModificationDrawing";
@@ -312,6 +313,7 @@ let originalRoutes = [
     , <Route path="/myTasks" component={myTasks} />
     , <Route path="/contractInfoAddEdit" component={contractInfoAddEdit} /> 
     , <Route path="/PaymentRequisitionList" component={PaymentRequisitionList} />
+    , <Route path="/ContractInfo" component={ContractInfo} />
     
 ];
     
