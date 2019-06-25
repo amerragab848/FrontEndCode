0e52691ec0a3a53a01ddc6e74ed8faafeaa86caa
Moutasem Abo Elsoud <m.aboelsoud@procoor.local>
2019-06-25 11:29:25 +0200

    get pull

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ class App extends Component {
 
   render() {
     const showComp = IsAuthorize ?
-      <div id="direction_warrper" dir={currentLanguage !== "ar" ? "ltr" : "rtl"}>
+      <div>
         <Menu />
         {Route}
       </div>
