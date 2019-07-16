d223b6b84ab4aea1b4cda8bef2f0f12b2e7140b8
m.ramdan <m.ramdan@procoor.local>
2019-07-16 11:11:19 +0200

    change some resources with ip config

--- src/IP_Configrations.js
+++ src/IP_Configrations.js
@@ -1,5 +1,4 @@
 var IP_CONFIG = {
-<<<<<<< HEAD
     BlobStorageContainerName: "project-files-demov4",
     acApp: "AC",
     accountCompanyId: "2",
@@ -18,23 +17,3 @@ var IP_CONFIG = {
     static: "https://demov4services.procoor.com/pm",
     downloads: "https://demov4services.procoor.com"
 };
-=======
-    "BlobStorageContainerName": "project-files-demov4",
-    "acApp": "AC",
-    "accountCompanyId": "2",
-    "azureStorage": true,
-    "canSendAlert": "false",
-    "cdn": "https://newgiza.azureedge.net",
-    "chatLocal": "https://demov4.procoor.com",
-    "chatStatic": "https://demov4.procoor.com",
-    "exportLocal": "https://demov4.procoor.com",
-    "exportStatic": "https://demov4.procoor.com",
-    "fmApp": "FM",
-    "hrApp": "HR",
-    "local": "http://172.30.1.17:8900",
-    "loginServer": "http://172.30.1.17:8901",
-    "pmApp": "PM",
-    "static": "http://172.30.1.17:8900",
-    "downloads": "http://172.30.1.17:8900"
-}
->>>>>>> 8f3e8501298cdc417c8c889892453361fd288e33
