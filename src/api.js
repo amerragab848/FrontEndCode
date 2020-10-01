0409be2a566de7e425285fd279a3623521f2b8af
mohamed ramadan <m.ramadan@procoor.com>
2020-10-01 11:34:31 +0200

    apply coomon log get data from core

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import CryptoJS from "crypto-js";
 import { toast } from "react-toastify";
 import Config from "./Services/Config";
- 
+
 let Authorization = localStorage.getItem("userToken");
 
 let modules = [{ api: 'PM/api/Procoor/', key: 1 }, { api: 'ExportAPI/api/ExportController/', key: 2 }, { api: 'ExportAPI/api/ExportController/', key: 3 }]
@@ -239,13 +239,14 @@ export default class Api {
     static post(route, params) {
         return this.xhr(route, params, "POST");
     }
- 
+
     static xhr(route, params, verb, moduleId) {
+        if (!moduleId) { moduleId = 1; }
+
         if (moduleId < 1) moduleId = 1;
         let apiPrefix = modules.find(x => x.key == moduleId);
-        const host = Config.getPublicConfiguartion().static + apiPrefix.api;// "/api/Procoor/";
-        //const host = Config.getPublicConfiguartion().static + "/PM/api/Procoor/";
-         const url = `${host}${route}`;
+        const host = Config.getPublicConfiguartion().static + apiPrefix.api; 
+        const url = `${host}${route}`;
         let json = null;
 
         let options = Object.assign(
