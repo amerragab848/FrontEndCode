9e9a80f26a926b456ce7bd4a20f2effd428dc9b3
ali salah <alisalah@procoor.com>
2020-12-23 01:39:59 -0800

    grouping in customGrid

--- src/Componants/Templates/Grid/CustomCommonLogGrid.js
+++ src/Componants/Templates/Grid/CustomCommonLogGrid.js
@@ -11,6 +11,8 @@ export default class CustomGrid extends Component {
     constructor(props) {
         super(props);
 
+
+
         this.state = {
             columns: this.props.cells,
             rows: this.props.data != null ? this.props.data : [],
@@ -48,6 +50,18 @@ export default class CustomGrid extends Component {
                 state[index + "-column"] = moment().format("DD/MM/YYYY");
             }
         });
+        
+        if(this.props.data[0].docDate !=undefined )
+        {
+            for( var i in this.props.data)
+            {
+                if(this.props.data[i].docDate !=null){
+                    this.props.data[i].docDate= this.props.data[i].docDate.split('T')[0]
+                }
+            }
+        }
+      
+        
 
         let ColumnsHideShow = this.props.cells
         for (var i in ColumnsHideShow) {
